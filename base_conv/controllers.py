4ab79ee378afcded29c501206df060b56ca30180
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-04 15:51:26 -0400

    Input validator for decimal

--- base_conv/controllers.py
+++ base_conv/controllers.py
@@ -1,3 +1,5 @@
+import re
+
 class BaseConvCtrl:
     def __init__(self, view):
         self._view = view
@@ -7,8 +9,13 @@ class BaseConvCtrl:
         pass
 
     def _decChanged(self):
-        self._view.hex.hexTextBox.document().setPlainText(self._view.dec.decTextBox.document().toPlainText())
-        self._view.bin.binTextBox.document().setPlainText(self._view.dec.decTextBox.document().toPlainText())
+        # Splits at anything not a 0-9 digit into a list
+        decValidator = re.split("[^0-9]+", self._view.dec.decTextBox.document().toPlainText())
+        # Joins to create one number again
+        validDecNumber = ''.join(decValidator)
+        self._view.dec.decTextBox.document().setPlainText(validDecNumber)
+        self._view.hex.hexTextBox.document().setPlainText(validDecNumber)
+        self._view.bin.binTextBox.document().setPlainText(validDecNumber)
     
     def _connectSignals(self):
         self._view.dec.decTextBox.textChanged.connect(lambda: self._decChanged())
