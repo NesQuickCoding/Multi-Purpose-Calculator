a29346e09d57b258d0177e1b24ebaf24e3a0372a
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-06 18:26:16 -0400

    started signal functionality

--- metric_conv/controllers.py
+++ metric_conv/controllers.py
@@ -1,3 +1,30 @@
 class MetricConvCtrl:
     def __init__(self, view, model):
         super().__init__()
+        self._view = view
+        self._models = model
+        self._connectComboBoxSignals()
+        self._connectTextSignals()
+    
+    def _leftTextChanged(self):
+        pass
+
+    def _rightTextChanged(self):
+        pass
+
+    def _connectComboBoxSignals(self):
+        pass
+
+    def _connectTextSignals(self):
+        self._view.leftTextEdit.textChanged.connect(self._leftTextChanged)
+        self._view.rightTextEdit.textChanged.connect(self._rightTextChanged)
+
+    def _disconnectTextSignals(self):
+        self._view.leftTextEdit.textChanged.disconnect()
+        self._view.rightTextEdit.textChanged.disconnect()
+    
+    def _setTextFields(self, string):
+        self._disconnectTextSignals()
+        self._view.leftTextEdit.setText(string)
+        self._view.rightTextEdit.setText(string)
+        self._connectTextSignals()
