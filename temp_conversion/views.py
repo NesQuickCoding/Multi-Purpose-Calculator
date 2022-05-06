a554dc84bf857a163c38880a114ab4df0b56f274
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-06 03:02:25 -0400

    bugfix: return value if convert to and from are the same unit

--- temp_conversion/views.py
+++ temp_conversion/views.py
@@ -18,19 +18,19 @@ class temp_Ui(QtWidgets.QWidget):
 
     def printButtonPressed(self):
         toggledButtons = [rb.text() for rb in self.radioButtons if rb.isChecked()]
-        val = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
-        answer = self.convert_temp(float(val.text()), toggledButtons[0][0], toggledButtons[1][0])
+        answer = self.convert_temp(float(self.input.text()), toggledButtons[0][0], toggledButtons[1][0])
         format_answer = "{:.2f}".format(answer)
         self.input.setText(str(format_answer))
         
-
     def convert_temp(self, val, original_unit, unit_to_convert_to):
         fahren_to_celcius = (val - 32)/1.8
         celcius_to_fahren = (val * 1.8) + 32
         fahren_to_kelvin = (val - 32) / 1.8 + 273.15
         kelvin_to_farhen = val * 1.8 - 459.67
 
-        if unit_to_convert_to == 'K':
+        if original_unit == unit_to_convert_to:
+            return val
+        elif unit_to_convert_to == 'K':
             if original_unit =='C':
                 return val + 273.15
             elif original_unit == 'F':
