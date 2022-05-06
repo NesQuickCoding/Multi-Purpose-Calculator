00778ff71af8ef23fcb57759ab3ed7430ffe682d
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-06 03:22:13 -0400

    bugfix: using QDoubleValidator for temp input validation

--- temp_conversion/views.py
+++ temp_conversion/views.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QDoubleValidator
 
 class temp_Ui(QtWidgets.QWidget):
     def __init__(self):
@@ -8,6 +9,7 @@ class temp_Ui(QtWidgets.QWidget):
         self.button = self.findChild(QtWidgets.QPushButton, 'enterButton') # Find the button
         self.button.clicked.connect(self.printButtonPressed) # Remember to pass the definition/method, not the return value!
         self.input = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
+        self.input.setValidator(QDoubleValidator(decimals=3))
         self.radioButtons = self.findChildren(QtWidgets.QRadioButton)
 
         # Set default radio buttons to prevent IndexError; toggleButtons would return en empty list otherwise
@@ -19,7 +21,7 @@ class temp_Ui(QtWidgets.QWidget):
     def printButtonPressed(self):
         toggledButtons = [rb.text() for rb in self.radioButtons if rb.isChecked()]
         answer = self.convert_temp(float(self.input.text()), toggledButtons[0][0], toggledButtons[1][0])
-        format_answer = "{:.2f}".format(answer)
+        format_answer = "{:.3f}".format(answer)
         self.input.setText(str(format_answer))
         
     def convert_temp(self, val, original_unit, unit_to_convert_to):
