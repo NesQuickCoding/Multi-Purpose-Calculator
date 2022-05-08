141da87b3bdc97ecd93a43999029b242a4bfba9e
NesQuickCoding <neslie.fernandez78@myhunter.cuny.edu>
2022-05-07 22:52:53 -0400

    feat: add QIntValidator and QRegExpValidator

--- birthday_conversion/views.py
+++ birthday_conversion/views.py
@@ -1,10 +1,13 @@
 from PyQt5 import QtWidgets, uic
 import sys
 from datetime import date
-from PyQt5.QtCore import QDate
+from PyQt5.QtCore import QDate, QRegExp
+from PyQt5.QtGui import QIntValidator, QRegExpValidator
+from PyQt5.QtWidgets import QCalendarWidget, QApplication, QDialog, QLabel
 
 """
 References:
+https://codingshiksha.com/python/python-pyqt5-gui-birthday-age-calculator-with-calendar-widget-desktop-app-full-project-for-beginners/
 https://pererapm.medium.com/find-out-how-many-seconds-old-you-are-using-python-and-explanation-of-code-ab2a48b9be3c
 https://www.geeksforgeeks.org/python-program-to-calculate-age-in-year/amp/
 https://stackoverflow.com/questions/68997443/calculate-age-in-day-month-and-year-from-birth-date-and-a-specific-day-using-py
@@ -15,34 +18,69 @@ class birthday_Ui(QtWidgets.QWidget):
         super(birthday_Ui, self).__init__()
         uic.loadUi('../Graphical-App/birthday_conversion/basic.ui', self)
 
+
+        # Our Enter Button
         self.button = self.findChild(QtWidgets.QPushButton, 'enterButton')
-        self.button.clicked.connect(self.printButtonPressed)
+        self.button.clicked.connect(self.enterButtonPressed)
+
+        # Valid Months
+        self.inputMonth = self.findChild(QtWidgets.QLineEdit, 'lineEdit_Month')
+        month_validator = QIntValidator(1,12)
+        self.inputMonth.setValidator(month_validator)
+
+        # Valid Days
+        self.inputDay = self.findChild(QtWidgets.QLineEdit, 'lineEdit_Day')
+        day_validator = QIntValidator(1,31)
+        self.inputDay.setValidator(day_validator)
+
+        # Valid Years
+        self.inputYear = self.findChild(QtWidgets.QLineEdit, 'lineEdit_Year')
+        reg_exp = QRegExp("^[19|20][0-9]{3}$")
+        year_validator = QRegExpValidator(reg_exp)
+        self.inputYear.setValidator(year_validator)
+
+        # Our Output Line
         self.input = self.findChild(QtWidgets.QLineEdit, 'lineEdit_Output')
+
+        # Our QComboBox
+        self.comboBox = self.findChild(QtWidgets.QComboBox, 'comboBox')
 
         self.show()
 
-    def printButtonPressed(self):
-        #radioButtons = self.findChildren(QtWidgets.QRadioButton)
-        #toggledButtons = [rb.text() for rb in radioButtons if rb.isChecked()]
-        self.clicked.connect(self.convert_birthday)
-        val = self.findChild(QtWidgets.QLineEdit, 'lineEdit_Output')
-        answer = convert_birthday(self)
+
+
+    def enterButtonPressed(self):
+
+        #toggledCombo = [cb.text() for cb in self.comboChoice]
+        self.calendar_input = [int(self.inputYear.text()), int(self.inputMonth.text()), int(self.inputDay.text())]
+        answer = self.getBirthday(self.comboChoice, self.calendar_input)
         #format_answer = "{:.2f}".format(answer)
-        self.input.setText(str(answer))
+
+        self.input.setText(answer)
+
+
+    def getBirthday(self, combo_input, calendar_input):
+
+        if combo_input == "Years":
+            return getYears(self, calendar_input)
+        elif combo_input == "Months":
+            return getMonths(self, calendar_input)
+        elif combo_input == 'Days':
+            return getDays(self, calendar_input)
+
 
 def getYears(self, born):
-    today = date.today()
+    c_month = born[0]
+    c_day = born[1]
+    c_year = born[2]
 
-    # Try Catch to check if we have a leap year
-    # Make sure current year is not leap year
-    try:
-        b_day = born.replace(year = today.year)
-    except ValueError:
-            b_day = born.replace(year = today.year, month = born.month + 1, day = 1)
-    if b_day > today:
-        return today.year - born.year - 1
-    else:
-        return today.year - born.year
+    today = date.today()
+    dob = date(c_year, c_day, c_month)
+    time_diff = today - dob
+    a_days = time_diff.days
+    a_year = int(a_days/365)
+
+    return a_year
 
 
 def getDays(self, born):
@@ -55,3 +93,4 @@ def getDays(self, born):
 def getMonths(self,born):
     c_days = getDays(born)
     c_months = int((c_days/365) * 12)
+        
