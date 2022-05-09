4505c2dba1b54e6f9973888caf62562fb11f4efa
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-09 18:53:28 -0400

    Doc Strings for metric_conv/models

--- metric_conv/models.py
+++ metric_conv/models.py
@@ -1,4 +1,24 @@
-def length_conversion(input_value, input_index, output_index):
+def length_conversion(input_value : float, input_index : int, output_index : int) -> float:
+    """
+    Calculates the conversion rate for length distances based on the input_index and 
+    output_index from from the MetricConvWidget.
+
+    The different units include Inches, Feet, Yards, Miles, Millimeters, Centimeters,
+    Meters, Kilometers
+
+    Parameters
+    ----------
+    input_value : float
+        The original numerical length value before conversion
+    input_index: int
+        The index of the length unit of the input value.
+    output_index: int
+        The index of the length unit that the input value will be converted to
+    Returns
+    -------
+    float
+        The converted length measurement from input units to output's units
+    """
     if input_index == output_index:
         return input_value
         
@@ -140,11 +160,31 @@ def length_conversion(input_value, input_index, output_index):
             return input_value / 1.609344
 
 def weight_conversion(input_value, input_index, output_index):
+    """
+    Calculates the conversion rate for different weights based on the input_index and 
+    output_index from from the MetricConvWidget.
+
+    The different units are Ounces, Pounds, Stone, Tons (Short), Milligrams, Grams,
+    Kilograms
+
+    Parameters
+    ----------
+    input_value : float
+        The original numerical weight value before conversion
+    input_index: int
+        The index of the weight unit of the input value.
+    output_index: int
+        The index of the weight unit that the input value will be converted to
+    Returns
+    -------
+    float
+        The converted weight measurement from input units to output's units
+    """
     if input_index == output_index:
         return input_value
     # Rows are input, column are output
     conversion_map = [
-        # Ounces         Pounds       Stone       Tons (Short)      Milligram   Gram        Kilogram
+        # Ounces         Pounds       Stone       Tons (Short)      Milligrams  Grams       Kilograms
         [1,	             1/16,	      1/224,	  1/32000,	        28349.5249, 28.3495249, 0.0283495249], # Ounces
         [16,	         1,	          1/14,	      1/2000,	        453592.4,   453.5923,   0.4535923],    # Pounds
         [224,	         16,	      1,	      7/1000,	        6350293,	6350.293,   6.350293],     # Stone
@@ -156,11 +196,30 @@ def weight_conversion(input_value, input_index, output_index):
     return input_value * conversion_map[input_index][output_index]
 
 def time_conversion(input_value, input_index, output_index):
+    """
+    Calculates the conversion rate for different time units based on the input_index and 
+    output_index from from the MetricConvWidget.
+
+    The different units are Milliseconds, Seconds, Minutes, Hours, Days, Months, and Years
+
+    Parameters
+    ----------
+    input_value : float
+        The original numerical time value before conversion
+    input_index: int
+        The index of the time unit of the input value.
+    output_index: int
+        The index of the time unit that the input value will be converted to
+    Returns
+    -------
+    float
+        The converted time measurement from input units to output's units
+    """
     if input_index == output_index:
         return input_value
 
     conversion_map = [
-        # MillisecondsSeconds  Minutes   Hours      Days        Months        Years
+        # MillisecondsSeconds   Minutes  Hours      Days        Months        Years
         [1,           1/1000,   1/60000, 1/3600000, 1/86400000, 1/2629800000, 1/31557600000], # Milliseconds
         [1000,        1,        1/60,    1/3600,    1/86400,    1/2629800,    1/31557600],    # Seconds
         [60000,       60,       1,       1/60,      1/1440,     1/43830,      1/525960],      # Minutes
@@ -172,6 +231,26 @@ def time_conversion(input_value, input_index, output_index):
     return input_value * conversion_map[input_index][output_index]
 
 def digital_space_conversion(input_value, input_index, output_index):
+    """
+    Calculates the conversion rate for different digital space units based on the input_index
+    and output_index from from the MetricConvWidget.
+
+    The different units are Bits, Bytes, Kilobytes, Megabytes, Gigabytes, Terabytes, Petabytes,
+    Exabytes, Zettabytes, Yottabytes
+
+    Parameters
+    ----------
+    input_value : float
+        The original numerical space value before conversion
+    input_index: int
+        The index of the space unit of the input value.
+    output_index: int
+        The index of the space unit that the input value will be converted to
+    Returns
+    -------
+    float
+        The converted digital spacce measurement from input units to output's units
+    """
     if input_index == output_index:
         return input_value
 
