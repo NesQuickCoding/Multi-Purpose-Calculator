6d520348be87487e80646ce3f659d0b0fd2a06de
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-10 01:24:16 -0400

    docs: docstrings for objects in base_conv/views.py

--- base_conv/views.py
+++ base_conv/views.py
@@ -2,22 +2,103 @@ from PyQt5.QtWidgets import QWidget, QRadioButton, QLabel, QPushButton, QVBoxLay
 from PyQt5.QtGui import QFont
 
 class NumBase(QWidget):
+    """
+    A QWidget that contains the foundation for three different base unit headers
+
+    Inherits all methods and attributes from QWidget
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    _CreateNumHeader(headerText):
+        Creates a QLabel with the text from headerText
+    _CreateNumTextBox():
+        Creates a QPlainTextEdit widget for input/output
+    """
     def __init__(self):
+        """
+        Constructs a NumBase widget, inheriting all of QWidget's methods and attributes
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        NumBase
+            Initialized NumBase object
+        """
         super().__init__()
 
     def _CreateNumHeader(self, headerText):
+        """
+        Constructs a QLabel header with headerText
+
+        Parameters
+        ----------
+        headerText : str
+            Input string
+
+        Returns
+        -------
+        QLabel
+            The QLabel header
+        """
         header = QLabel(headerText)
         header.setFont(QFont('Arial', 14))
         return header
     
     def _CreateNumTextBox(self):
+        """
+        Constructs a QPlainTextEdit
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        QPlainTextEdit
+            The QPlanTextEdit to be used for input/output
+        """
         textbox = QPlainTextEdit()
         textbox.setFont(QFont('Terminal', 10))
         textbox.document().setPlainText("0")
         return textbox
 
 class DecBase(NumBase):
+    """
+    A decimal base input/output widget
+
+    Inherits all methods and attributes from NumBase
+
+    Attributes
+    ----------
+    decHeader : QLabel
+        Text for the header
+    decTextBox : QPlainTextEdit
+        Input/output text field
+
+    Methods
+    -------
+    None
+    """
     def __init__(self):
+        """
+        Initializer for DecBase
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        DecBase
+            Initalized DecBase object
+        """
         layout = QVBoxLayout()
         super().__init__()        
         self.decHeader = self._CreateNumHeader("Decimal")
@@ -27,17 +108,73 @@ class DecBase(NumBase):
         self.setLayout(layout)
 
 class HexBase(NumBase):
+    """
+    A hexadecimal base input/output widget
+
+    Inherits all methods and attributes from NumBase
+
+    Attributes
+    ----------
+    hexHeader : QLabel
+        Text for the header
+    hexTextBox : QPlainTextEdit
+        Input/output text field
+
+    Methods
+    -------
+    None
+    """
     def __init__(self):
+        """
+        Initializer for DecBase
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        HexBase
+            Initalized HexBase object
+        """
         super().__init__()
         layout = QVBoxLayout()
-        self.hexHeader = self._CreateNumHeader("Hexidecimal")
+        self.hexHeader = self._CreateNumHeader("Hexadecimal")
         self.hexTextBox = self._CreateNumTextBox()
         layout.addWidget(self.hexHeader)
         layout.addWidget(self.hexTextBox)
         self.setLayout(layout)        
 
 class BinBase(NumBase):
+    """
+    A binary base input/output widget
+
+    Inherits all methods and attributes from NumBase
+
+    Attributes
+    ----------
+    binHeader : QLabel
+        Text for the header
+    binTextBox : QPlainTextEdit
+        Input/output text field
+
+    Methods
+    -------
+    None
+    """
     def __init__(self):
+        """
+        Initializer for BinBase
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        BinBase
+            Initalized BinBase object
+        """
         super().__init__()  
         layout = QVBoxLayout()
         self.binHeader = self._CreateNumHeader("Binary")
@@ -47,14 +184,56 @@ class BinBase(NumBase):
         self.setLayout(layout)      
 
 class BaseConvUI(QWidget):
+    """
+    A QWidget creates the three main number bases of decimal, hexadecimal, and binary.
+    Also creates the layout for signed/unsigned QRadioButtons, a negate QPushButton,
+    and QComboBox for bit-width selection.
+
+    Inherits all methods and attributes from QWidget
+
+    Attributes
+    ----------
+    unsignedRadio : QRadioButton
+        Radiobutton input for unsigned
+    signedRadio : QRadioButton
+        Radiobutton input for signed
+    negateBUtton : QPushButton
+        button to negate values
+    bitDropBox : QComboBox
+        Dropbox menu selection for 8, 16, 32, or 64 bit widths
+    dec : DecBase
+        DecBase Widget
+    hex : HexBase
+        HexBase Widget
+    bin : BinBase
+        BinBase Widget
+
+    Methods
+    -------
+    _createBitLengthBox():
+        Internal method to create QComboBox with 8-64 bit options
+    """
     def __init__(self):
+        """
+        Initializer for BaseConvUI. Creates all QWidgets and layouts
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        BaseConvUI
+            Initalized BaseConvUI view
+        """
         super().__init__()
         layout = QVBoxLayout()
         bitLayout = QHBoxLayout()
-        self.unSignedRadio = self._createSignedRadios("Unsigned")
+        self.unSignedRadio = QRadioButton("Unsigned")
         self.unSignedRadio.setChecked(True)
-        self.signedRadio = self._createSignedRadios("Signed")
-        self.negateButton = self._createNegateButton()
+        self.signedRadio = QRadioButton("Unsigned")
+        self.negateButton = QPushButton("Negate")
+        self.negateButton.setEnabled(False)
         self.bitDropBox = self._createBitLengthBox()
         bitLayout.addWidget(self.unSignedRadio)
         bitLayout.addWidget(self.signedRadio)
@@ -68,17 +247,20 @@ class BaseConvUI(QWidget):
         self.bin = BinBase()
         layout.addWidget(self.bin)
         self.setLayout(layout)
-    
-    def _createSignedRadios(self, text):
-        radio = QRadioButton(text)
-        return radio
-
-    def _createNegateButton(self):
-        button = QPushButton("Negate")
-        button.setEnabled(False)
-        return button
 
     def _createBitLengthBox(self):
+        """
+        Internal method for creating a QComboBox with 8-64 bit-width options
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        QComboBox
+            Dropbox of various bit-widths
+        """
         bitDropBox = QComboBox()
         bitDropBox.setObjectName("bitDropBox")
         bitDropBox.addItems(["QWord - 64bit", "DWord - 32bit", "Word - 16bit", "Byte - 8bit"])
