2e1d6e694f698f902ad58db84f7fe4d4f714bbe0
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-10 02:23:33 -0400

    refact: radiobuttons is class member, convert_ascii is class method

--- ascii_conversion/views.py
+++ ascii_conversion/views.py
@@ -1,6 +1,4 @@
 from PyQt5 import QtWidgets, uic
-import sys
-
 
 class ascii_Ui(QtWidgets.QWidget):
     def __init__(self):
@@ -8,60 +6,57 @@ class ascii_Ui(QtWidgets.QWidget):
         uic.loadUi('../Graphical-App/ascii_conversion/basic.ui', self)
 
         self.button = self.findChild(QtWidgets.QPushButton, 'enterButton')
-        self.button.clicked.connect(
-            self.printButtonPressed)
+        self.button.clicked.connect(self.printButtonPressed)
         self.input = self.findChild(QtWidgets.QLineEdit, 'lineEdit_Output')
+        self.radioButtons = self.findChildren(QtWidgets.QRadioButton)
+
         self.show()
 
     def printButtonPressed(self):
-        radioButtons = self.findChildren(QtWidgets.QRadioButton)
-        toggledButtons = [rb.text() for rb in radioButtons if rb.isChecked()]
+        toggledButtons = [rb.text() for rb in self.radioButtons if rb.isChecked()]
         val = self.findChild(QtWidgets.QLineEdit, 'lineEdit_Input')
-        answer = convert_ascii(self, val.text(), toggledButtons[0][0], toggledButtons[1][0])
+        answer = self.convert_ascii(self, val.text(), toggledButtons[0][0], toggledButtons[1][0])
         self.input.setText(str(answer))
 
-
-
-
-def convert_ascii(self,val, input_unit, output_unit):
-    try:
-        if input_unit == 'C':
-            if output_unit == 'D':
-                return ord(val)
-            elif output_unit == 'H':
-                return hex(ord(val))
-            else:
-                return val
-
-        elif input_unit == 'D':
-            try:
-
-                if output_unit == 'C':
-                    val = int(val)
-                    return chr(val)
-
-                # Problem with this conversion H
+    def convert_ascii(self, val, input_unit, output_unit):
+        try:
+            if input_unit == 'C':
+                if output_unit == 'D':
+                    return ord(val)
                 elif output_unit == 'H':
-                    return hex(int(val))
+                    return hex(ord(val))
                 else:
                     return val
-            except ValueError:
-                return "Invalid Input"
-        elif input_unit == 'H':
-            if output_unit == 'D':
-                return int(val,16)
-            elif output_unit == 'C':
-                val_str = val[2:]
-                bytes_obj = bytes.fromhex(val_str)
-                ascii_str = bytes_obj.decode("ASCII")
-                return ascii_str
-            else:
-                return val
-        else:
-            return "Invalid Input"
 
-    except ValueError:
-        return "Invalid Input"
+            elif input_unit == 'D':
+                try:
+
+                    if output_unit == 'C':
+                        val = int(val)
+                        return chr(val)
+
+                    # Problem with this conversion H
+                    elif output_unit == 'H':
+                        return hex(int(val))
+                    else:
+                        return val
+                except ValueError:
+                    return "Invalid Input"
+            elif input_unit == 'H':
+                if output_unit == 'D':
+                    return int(val,16)
+                elif output_unit == 'C':
+                    val_str = val[2:]
+                    bytes_obj = bytes.fromhex(val_str)
+                    ascii_str = bytes_obj.decode("ASCII")
+                    return ascii_str
+                else:
+                    return val
+            else:
+                return "Invalid Input"
+
+        except ValueError:
+            return "Invalid Input"
 
 
 
@@ -92,4 +87,3 @@ def convert_ascii(self,val, input_unit, output_unit):
 #print(ans2)
 #ans3 = convert_ascii("0x45",'H','H')
 #print(ans3)
-
