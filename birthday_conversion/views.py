eaa296e0acdd41024443549791500d2a114093ee
NesQuickCoding <neslie.fernandez78@myhunter.cuny.edu>
2022-05-11 05:25:10 -0400

    docs: docstring for birthday_Ui class

--- birthday_conversion/views.py
+++ birthday_conversion/views.py
@@ -12,7 +12,83 @@ https://stackoverflow.com/questions/68997443/calculate-age-in-day-month-and-year
 """
 
 class birthday_Ui(QtWidgets.QWidget):
+    """
+        Creates a QWidget that converts a valid birthdate / date to years, days, or months
+        based on users combo box choice.
+
+        Inhereits all methods and attributes from QWidget
+
+        Attributes
+        ----------
+         clearButton:
+            clears input and output text fields.
+         enterButton:
+            button to initiate the converison.
+        input : QLineEdit
+            lineEdit_Day:
+                for int input representing a calendar 1 or 2 digit day
+                ranging from 1 to 31 based on months.
+
+            lineEdit_Month:
+                for int input representing a 1 or 2 digit number
+                for months ranging from 1 to 12.
+
+            lineEdit_Year:
+                for int input having 4 digits representing years.
+
+            Input text field
+                QComboBox
+            User input based on listed items (Years, Months, Days)
+
+        validators : [QValidators]
+            Used to determine which validations to apply to the input based
+            on users input.
+        output : QLineEdit
+            Output text field
+        comboBox = [QComboBox]
+            A list of all the box options.
+            0 - Years (Input)
+            1 - Months (Input)
+            2 - Days (Input)
+
+        Methods
+        -------
+
+        enterButtonPressed():
+            Sends a list input that contains [month,day,year] from user input
+            to get_birthday then sets the text of output to the results
+
+        def get_birthday(self, combo_input, calendar_input):
+            Takes in  a list and a choice input from the combo box for the type of conversion the user wants
+            Years, Months, Days and returns the conversion in text.
+
+        def get_years(month,day,year):
+            Takes in a tuple of int values that represents a birthdate and converts it to
+            years based on present day.
+
+        def get_days(month,day,year):
+            Takes in a tuple of int values that represents a birthdate and converts it to
+            days based on present day.
+
+        def get_months(month,day,year):
+            Takes in a tuple of int values that represents a birthdate and converts it to
+            months based on present day.
+
+
+        """
     def __init__(self):
+        """
+          Initializes the birthday_Ui, including it's layout from basic.ui and attributes
+
+          Parameters
+          ----------
+          None
+
+          Returns
+          -------
+          birthday_Ui
+              Newly constructed widget
+          """
         super(birthday_Ui, self).__init__()
         uic.loadUi('../Graphical-App/birthday_conversion/basic.ui', self)
 
@@ -40,6 +116,18 @@ class birthday_Ui(QtWidgets.QWidget):
 
 
     def enterButtonPressed(self):
+        """
+         Sends a list input that contains [month,day,year] from user input
+         to get_birthday then sets the text of output to the results
+
+         Parameters
+         ----------
+         None
+
+         Returns
+         -------
+         None
+         """
         self.lineEdit_Output.clear()
         current_choice = self.comboBox.currentText()
         month = int(self.inputMonth.text())
@@ -51,6 +139,28 @@ class birthday_Ui(QtWidgets.QWidget):
 
 
     def get_birthday(self, combo_input, calendar_input):
+        """
+        Takes in  a list and a choice input from the combo box for the type of conversion the user wants
+        Years, Months, Days and returns the conversion in text.
+
+        Parameters
+        ----------
+
+        combo_input: list, int
+            Month, Day, and Year input value to convert
+        calender_input:
+            Input choice from the combo box depending on the conversion user picks.
+
+        Raises
+        ------
+         ValueError
+            If the user entered a date that is considered invalid.
+
+        Returns
+        -------
+            str that represents the int converison
+
+        """
         try:
             c_month = calendar_input[0]
             c_day = calendar_input[1]
@@ -70,6 +180,23 @@ class birthday_Ui(QtWidgets.QWidget):
 
 
 def get_years(month,day,year):
+    """
+    Takes in a tuple of int values that represents a birthdate and converts it to
+    years based on present day.
+
+    Parameters
+    ----------
+    month : int
+        Input value to convert
+    day:   int
+        Input value to convert
+    year: int
+        Input value to convert
+
+    Returns
+    -------
+        int value
+    """
     today = date.today()
     dob = date(year, month, day)
     time_diff = today - dob
@@ -79,6 +206,23 @@ def get_years(month,day,year):
 
 
 def get_days(month,day,year):
+    """
+    Takes in a tuple of int values that represents a birthdate and converts it to
+    days based on present day.
+
+    Parameters
+    ----------
+    month : int
+        Input value to convert
+    day:   int
+        Input value to convert
+    year: int
+        Input value to convert
+
+    Returns
+    -------
+        int value
+    """
     dob = date(year, month, day)
     today = date.today()
     time_diff = today - dob
@@ -87,6 +231,23 @@ def get_days(month,day,year):
 
 
 def get_months(month,day,year):
+    """
+    Takes in a tuple of int values that represents a birthdate and converts it to
+    months based on present day.
+
+    Parameters
+    ----------
+    month : int
+        Input value to convert
+    day:   int
+        Input value to convert
+    year: int
+        Input value to convert
+
+    Returns
+    -------
+        int value
+    """
     dob = date(year, month, day)
     today = date.today()
     time_diff = today - dob
