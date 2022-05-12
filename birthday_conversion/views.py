3572287b7d3af0ce1a449237607cd0adeba029bb
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-12 01:17:43 -0400

    feature: input validation checks for proper limits are based on year and month

--- birthday_conversion/views.py
+++ birthday_conversion/views.py
@@ -35,8 +35,68 @@ class birthday_Ui(QtWidgets.QWidget):
         reg_exp = QRegExp("^[19|20][0-9]{3}$")
         year_validator = QRegExpValidator(reg_exp)
         self.inputYear.setValidator(year_validator)
+        self.yearLimit = date.today().year
+
+        # signal connectors for inputs
+        self.inputDay.textChanged.connect(self._inputCheck)
+        self.inputMonth.textChanged.connect(self._inputCheck)
+        self.inputYear.textChanged.connect(self._inputCheck)
+
         self.show()
 
+    def _inputCheck(self):
+        # run checks only if there's a value for month
+        if self.inputMonth.text():
+            if int(self.inputMonth.text()) <= 0:
+                self.inputMonth.setText("")
+            elif int(self.inputMonth.text()) > 12:
+                self.inputMonth.setText("12")
+        
+        # run checks only if there's a value for year
+        if self.inputYear.text():
+            if int(self.inputYear.text()) <= 0:
+                self.inputYear.setText("")
+            elif int(self.inputYear.text()) > self.yearLimit:
+                self.inputYear.setText(str(self.yearLimit))
+        
+        # run checks only if there's a value for year
+        if self.inputDay.text():
+            if int(self.inputDay.text()) <= 0:
+                self.inputDay.setText("")
+            else:
+                # since the limit of days depends on the month and year,
+                # we'll run several different cases
+                # using try/except value error in case month does not have input
+                try:
+                    # check limit if month as 31 days
+                    if int(self.inputMonth.text()) in [1, 3, 5, 7, 8, 10, 12] and int(self.inputDay.text()) > 31:
+                        self.inputDay.setText("31")
+                    
+                    # check for limit for feburary
+                    elif int(self.inputMonth.text()) == 2:
+
+                        # using try/except for value error in case year has no input
+                        try:
+                            # check for leap year
+                            if int(self.inputYear.text()) % 4 == 0 and int(self.inputDay.text()) > 29:
+                                self.inputDay.setText("29")
+                            elif int(self.inputYear.text()) % 4 != 0 and int(self.inputDay.text()) > 28:
+                                self.inputDay.setText("28")
+                        except ValueError:
+                            # assume 28 days
+                            if int(self.inputDay.text()) > 28:
+                                self.inputDay.setText("28")
+                    
+                    # all other months have 30 days
+                    else:
+                        if int(self.inputDay.text()) > 30:
+                            self.inputDay.setText("30")
+                
+                except ValueError:
+                    # assume 31 days
+                    if int(self.inputDay.text()) > 31:
+                            self.inputDay.setText("31")
+        
     def enterButtonPressed(self):
         try:
             self.lineEdit_Output.clear()
@@ -75,14 +135,12 @@ def get_years(month,day,year):
     a_years = int(a_days/365)
     return a_years
 
-
 def get_days(month,day,year):
     dob = date(year, month, day)
     today = date.today()
     time_diff = today - dob
     a_days = time_diff.days
     return a_days
-
 
 def get_months(month,day,year):
     dob = date(year, month, day)
