6a14d0c5e5a6d0b7399a9a9ce0861b72b4cb1788
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-05-13 03:48:06 -0400

    reverted test comments

--- base_conv/test.py
+++ base_conv/test.py
@@ -2,9 +2,9 @@ import sys
 
 from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget, QMainWindow
 
-# from views import BaseConvUI
-# import models
-# from controllers import BaseConvCtrl
+from views import BaseConvUI
+import models
+from controllers import BaseConvCtrl
 
 class TestWindowBaseConv(QMainWindow):
     """
